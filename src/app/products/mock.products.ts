66ef0fc203dad8d0b938afb8f6f46928137dfa41
IGNACIO <megalucio@Ruth-Craigs-MacBook-Pro.local>
2016-10-03 15:19:30 -0500

    Listing products from a mock constant

---
+++ src/app/products/mock.products.ts
@@ -0,0 +1,14 @@
+import { Product } from './product';
+
+export const PRODUCTS: Product[] = [
+  { id: 11, name: 'Melon' },
+  { id: 12, name: 'Manzana' },
+  { id: 13, name: 'Sandia' },
+  { id: 14, name: 'Piña' },
+  { id: 15, name: 'Melocoton' },
+  { id: 16, name: 'Pera' },
+  { id: 17, name: 'Uva' },
+  { id: 18, name: 'Platano' },
+  { id: 19, name: 'Guava' },
+  { id: 20, name: 'Naranja' }
+];
---
+++ src/app/products/product.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+import { Product } from './product';
+
+import { PRODUCTS } from './mock.products';
+
+@Injectable()
+export class ProductService {
+  getProducts(): Promise<Product[]> {
+    return Promise.resolve(PRODUCTS);
+  }
+}
