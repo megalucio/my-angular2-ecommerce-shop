7bd6356a1ded9e34c77a9334d2b8f6e761eb84d7
IGNACIO <megalucio@Ruth-Craigs-MacBook-Pro.local>
2016-10-06 17:44:12 -0500

    Adding an empty cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { HttpModule, JsonpModule } from '@angular/http';
 import {About} from './components/about/about';
 import {Home} from './components/home/home';
 import {Products} from './components/products/products';
+import {Cart} from './components/cart/cart';
 import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 
 @NgModule({
-  declarations: [AppComponent, About, Home, Products],
+  declarations: [AppComponent, About, Home, Products, Cart],
   imports     : [BrowserModule, FormsModule, HttpModule, JsonpModule, RouterModule.forRoot(rootRouterConfig)],
   providers   : [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap   : [AppComponent]
---
+++ src/app/components/cart/cart.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cart',
+  styleUrls: ['./cart.css'],
+  templateUrl: './cart.html',
+})
+export class Cart {
+
+  constructor() { }
+                   
+  ngOnInit(): void {
+  }
+}
